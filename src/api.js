d11f941737b72eeb74cc13244309211035ba87b0
Towhid Sarker <133368387+towhid-01@users.noreply.github.com>
2024-05-28 15:11:22 +0600

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,18 +2,19 @@ import axios from "axios";
 import { LANGUAGE_VERSIONS } from "./constants";
 
 const API = axios.create({
-  baseURL: "https://emkc.org/api/v2/piston",
+    baseURL: "https://emkc.org/api/v2/piston",
 });
 
-export const executeCode = async (language, sourceCode) => {
-  const response = await API.post("/execute", {
-    language: language,
-    version: LANGUAGE_VERSIONS[language],
-    files: [
-      {
-        content: sourceCode,
-      },
-    ],
-  });
-  return response.data;
+export const executeCode = async (language, sourceCode, input) => {
+    const response = await API.post("/execute", {
+        language: language,
+        version: LANGUAGE_VERSIONS[language],
+        files: [
+            {
+                content: sourceCode,
+            },
+        ],
+        stdin: input, 
+    });
+    return response.data;
 };
